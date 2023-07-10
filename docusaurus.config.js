51c07c9156373534a44cc94d3138235a644a49cf
Jonas <me@pentoxi.de>
2023-07-11 00:07:55 +0200

    Update URL - GH Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Native-Servers-com.github.io',
+  url: 'https://community.native-servers.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
